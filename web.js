4ab76d105df32f3d87606ff8f23b07ba6a1adfd4
Greenhand <cooperateonline@gmail.com>
2013-07-12 15:40:53 +0000

    updated

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World 2!');
+    var fileContent = fs.readFileSync('index.html', 'utf-8',function (err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(data);
+    });
+    var fileContentString = fileContent.toString('utf8');
+    response.send(fileContentString);
 });
 
 var port = process.env.PORT || 5000;
